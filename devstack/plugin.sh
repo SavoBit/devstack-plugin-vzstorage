1d436fba8333a03edc481c66b24c9508f4178e93
Evgeny Antyshev <eantyshev@virtuozzo.com>
2016-04-28 14:56:30 +0000

    just what it needs

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # devstack/plugin.sh
-# Triggers glusterfs specific functions to install and configure GlusterFS
+# Devstack plugin to install and configure Vzstorage
 
 # Dependencies:
 #
@@ -10,92 +10,31 @@
 
 # ``stack.sh`` calls the entry points in this order:
 #
-# - install_glusterfs
-# - start_glusterfs
-# - configure_cinder_backend_glusterfs
-# - configure_cinder_backup_backend_glusterfs
-# - configure_glance_backend_glusterfs
-# - configure_nova_backend_glusterfs
-# - configure_manila_backend_glusterfs
-# - stop_glusterfs
-# - cleanup_glusterfs
-
-# configure_cinder_backend_vzstorage - Set config files, create data dirs, etc
-function configure_cinder_backend_vzstorage {
-    local be_name=$1
-    iniset $CINDER_CONF $be_name volume_backend_name $be_name
-    iniset $CINDER_CONF $be_name volume_driver "cinder.volume.drivers.vzstorage.VZStorageDriver"
-    iniset $CINDER_CONF $be_name vzstorage_shares_config "$CINDER_CONF_DIR/vzstorage-shares-$be_name.conf"
-
-    CINDER_VZSTORAGE_CLUSTERS="$CINDER_VZSTORAGE_CLUSTER_NAME [\"-u\", \"stack\", \"-g\", \"qemu\", \"-m\", \"0770\"]"
-    echo "$CINDER_VZSTORAGE_CLUSTERS" | tee "$CINDER_CONF_DIR/vzstorage-shares-$be_name.conf"
-}
-# init_cinder_backend_vzstorage - Initialize minimalistic vzstorage cluster
-# init_cinder_backend_vzstorage $be_name
-function init_cinder_backend_vzstorage {
-    local be_name=$1
-
-    set -e
-    CLUSTER_NAME=$CINDER_VZSTORAGE_CLUSTER_NAME
-    PSTORAGE_PKGS="pstorage-chunk-server pstorage-client pstorage-ctl \
-                   pstorage-libs-shared pstorage-metadata-server"
-    if [[ "$os_VENDOR" == "CentOS" ]]; then
-        sudo rpm -i http://download.pstorage.parallels.com/standalone/packages/rhel/7/pstorage-release.noarch.rpm
-        PSTORAGE_PKGS=$PSTORAGE_PKGS pstorage-kmod
-    elif [[ "$os_VENDOR" == "Virtuozzo" ]]; then
-        echo "Running on Virtuozzo distribution, it requires no other kernel modules"
-        # Hack to restart vcmmd after numpy updated
-        sudo systemctl restart vcmmd.service
-    else
-        die $LINENO "Vzstorage is supported on CentOS and Virtuozzo distributions only"
-    fi
-
-    sudo yum install -y $PSTORAGE_PKGS
-    [ -d /pstorage ] || sudo mkdir /pstorage
-
-    echo PASSWORD | sudo pstorage -c $CLUSTER_NAME make-mds -I -a 127.0.0.1 -r /pstorage/$CLUSTER_NAME-mds -P
-    sudo service pstorage-mdsd start
-    sudo chkconfig pstorage-mdsd on
-
-    sudo pstorage -c $CLUSTER_NAME make-cs -r /pstorage/$CLUSTER_NAME-cs
-    sudo service pstorage-csd start
-    sudo chkconfig pstorage-csd on
-
-    echo 127.0.0.1 | sudo tee /etc/pstorage/clusters/$CLUSTER_NAME/bs.list
-
-    set +e
-}
+# - install_vzstorage
+# - start_vzstorage
+# - configure_cinder_backend_vzstorage
+# - configure_glance_backend_vzstorage
+# - configure_nova_backend_vzstorage
+# - stop_vzstorage
+# - cleanup_vzstorage
 
 
 if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
-    echo_summary "Installing GlusterFS 3.7"
-    install_glusterfs 3.7
+    echo_summary "Installing Vzstorage"
+    install_vzstorage
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-    if is_service_enabled glance && [[ "$CONFIGURE_GLUSTERFS_GLANCE" == "True" ]]; then
-        echo_summary "Configuring GlusterFS as a backend for Glance"
-        configure_glance_backend_glusterfs
+    if is_service_enabled cinder && [[ "$CONFIGURE_VZSTORAGE_CINDER" == "True" ]]; then
+        echo_summary "Configuring Vzstorage as a backend for Nova"
+        configure_cinder_backend_vzstorage
     fi
-    if is_service_enabled nova && [[ "$CONFIGURE_GLUSTERFS_NOVA" == "True" ]]; then
-        echo_summary "Configuring GlusterFS as a backend for Nova"
-        configure_nova_backend_glusterfs
-    fi
-    if is_service_enabled manila && [[ "$CONFIGURE_GLUSTERFS_MANILA" == "True" ]]; then
-        echo_summary "Configuring GlusterFS as a backend for Manila"
-        configure_manila_backend_glusterfs
-    fi
-elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
-    # Changing file permissions of glusterfs logs.
-    # This avoids creation of zero sized glusterfs log files while running CI job (Bug: 1455951).
-    sudo chmod 755 -R /var/log/glusterfs/
 fi
 
 if [[ "$1" == "unstack" ]]; then
-    cleanup_glusterfs
-    stop_glusterfs
+    cleanup_vzstorage
 fi
 
 if [[ "$1" == "clean" ]]; then
-    cleanup_glusterfs
+    cleanup_vzstorage
 fi
 
 ## Local variables:
---
+++ devstack/vzstorage-functions.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# devstack/vzstorage-functions.sh
+# Functions to control the installation and configuration of the Vzstorage
+
+# Installs Vzstorage packages, confiugures minimal functioning setup 
+# Triggered from devstack/plugin.sh as part of devstack "pre-install"
+function install_vzstorage {
+    set -e
+    CLUSTER_NAME=$VZSTORAGE_CLUSTER_NAME
+    PSTORAGE_PKGS="pstorage-chunk-server pstorage-client pstorage-ctl \
+                   pstorage-libs-shared pstorage-metadata-server"
+    if [[ "$os_VENDOR" == "CentOS" ]]; then
+        sudo rpm -i $PSTORAGE_STANDALONE_REPO_PKG
+        PSTORAGE_PKGS=$PSTORAGE_PKGS pstorage-kmod
+    elif [[ "$os_VENDOR" == "Virtuozzo" ]]; then
+        echo "Running on Virtuozzo distribution, it requires no other kernel modules"
+        # Hack to restart vcmmd after numpy updated
+        sudo systemctl restart vcmmd.service
+    else
+        die $LINENO "Vzstorage is supported on CentOS and Virtuozzo distributions only"
+    fi
+
+    #sudo yum install -y $PSTORAGE_PKGS
+    install_packages $PSTORAGE_PKGS
+    [ -d $VZSTORAGE_DATA_DIR ] || sudo mkdir $VZSTORAGE_DATA_DIR
+
+    echo PASSWORD | sudo pstorage -c $CLUSTER_NAME make-mds -I -a 127.0.0.1 \
+        -r $VZSTORAGE_DATA_DIR/$CLUSTER_NAME-mds -P
+    sudo service pstorage-mdsd start
+    sudo chkconfig pstorage-mdsd on
+
+    sudo pstorage -c $CLUSTER_NAME make-cs -r $VZSTORAGE_DATA_DIR/$CLUSTER_NAME-cs
+    sudo service pstorage-csd start
+    sudo chkconfig pstorage-csd on
+
+    echo 127.0.0.1 | sudo tee /etc/pstorage/clusters/$CLUSTER_NAME/bs.list
+
+    set +e
+}
+
+# Cleanup Vzstorage
+# Triggered from devstack/plugin.sh as part of devstack "clean"
+function cleanup_vzstorage {
+
+    sudo service pstorage-mdsd stop
+    sudo service pstorage-csd stop
+    sudo rm -rf /etc/pstorage/clusters/*
+    sudo rm -rf ${VZSTORAGE_DATA_DIR}
+}
+
+
+# Configure Vzstorage as a backend for Cinder
+# Triggered from stack.sh
+# configure_cinder_backend_vzstorage
+function configure_cinder_backend_vzstorage {
+    local be_name=$1
+    iniset $CINDER_CONF DEFAULT os_privileged_user_auth_url $KEYSTONE_AUTH_URI/v2.0
+    iniset $CINDER_CONF $be_name volume_backend_name $be_name
+    iniset $CINDER_CONF $be_name volume_driver "cinder.volume.drivers.vzstorage.VZStorageDriver"
+    iniset $CINDER_CONF $be_name vzstorage_shares_config "$CINDER_CONF_DIR/vzstorage-shares-$be_name.conf"
+
+    CINDER_VZSTORAGE_CLUSTERS="$VZSTORAGE_CLUSTER_NAME [\"-u\", \"stack\", \"-g\", \"qemu\", \"-m\", \"0770\"]"
+    echo "$CINDER_VZSTORAGE_CLUSTERS" | tee "$CINDER_CONF_DIR/vzstorage-shares-$be_name.conf"
+}
